049bf4aabe19d7b1fac05c17c636b61368fc556a
Rohit Walecha <rohitw@Rohits-MacBook-Pro-2.local>
2026-04-22 00:03:14 +0530

    Go profit calcultor using approach elaborating using of functions and return types

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -2,26 +2,30 @@ package main
 
 import "fmt"
 
+// Go profit calcultor using approach elaborating using of functions and return types
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
-	fmt.Print("Enter revenue : ")
-	fmt.Scan(&revenue)
+	revenue := scanInput("Enter revenue : ")
+	expenses := scanInput("Enter expenses : ")
+	taxRate := scanInput("Enter taxRate : ")
 
-	fmt.Print("Enter expenses : ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Enter taxRate : ")
-	fmt.Scan(&taxRate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate)
-	ratio := ebt / profit
+	ebt, profit, ratio := calculateExpenses(revenue, expenses, taxRate)
 
 	fmt.Printf("Earnings Before Tax : %.2f\n", ebt)
 	fmt.Printf("Profit : %.2f\n", profit)
 	fmt.Printf("Ratio EBT/Profit : %.2f\n", ratio)
 
+}
+
+func calculateExpenses(revenue float64, expenses float64, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func scanInput(inputString string) (inputValue float64) {
+	fmt.Print(inputString)
+	fmt.Scan(&inputValue)
+	return
 }
