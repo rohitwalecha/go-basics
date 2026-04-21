dccf2dd01f0cebd966a648a60addf40ec02f7be3
Rohit Walecha <rohitw@Rohits-MacBook-Pro-2.local>
2026-04-22 00:43:41 +0530

    Control structures demonstration of user fetching account balance and depositing amount in the account  and for loop

--- control-structures/control_structures.go
+++ control-structures/control_structures.go
@@ -10,18 +10,28 @@ func main() {
 	var choice int
 
 	fmt.Println("Welcom to Go Bank.")
-	fmt.Println("Press 1 -> Check Balance.")
-	fmt.Println("Press 2 -> Deposit Amount")
+	for {
+		fmt.Println("Press 1 -> Check Balance.")
+		fmt.Println("Press 2 -> Deposit Amount")
+		fmt.Println("Press 3 -> Go Back.")
+		fmt.Println("Press 4 -> Exit.")
 
-	fmt.Scan(&choice)
+		fmt.Scan(&choice)
 
-	if choice == 1 {
-		fmt.Printf("Your account balance is %.2f\n", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("Enter Amount to Deposit : ")
-		fmt.Scan(&depositedAmount)
-		accountBalance += depositedAmount
-		fmt.Println("Thanks for making the transaction.")
-		fmt.Printf("Your account balance is %.2f\n", accountBalance)
+		if choice == 1 {
+			fmt.Printf("Your account balance is %.2f\n", accountBalance)
+		} else if choice == 2 {
+			fmt.Print("Enter Amount to Deposit : ")
+			fmt.Scan(&depositedAmount)
+			accountBalance += depositedAmount
+			fmt.Println("Thanks for making the transaction.")
+			fmt.Printf("Your account balance is %.2f\n", accountBalance)
+		} else if choice == 3 {
+			continue
+		} else {
+			break
+		}
+
 	}
+	fmt.Println("Thanks for Using Go Bank, Have a nice day :) ")
 }
